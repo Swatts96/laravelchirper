679a5f8a6898460858de3479114f0de0e7bf2beb
Sam <w0292233@nscc.ca>
2024-11-21 14:13:56 -0400

    created empty Like controller and imported. Added routes for post and delete to like and unlike

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,8 @@
 use App\Http\Controllers\ProfileController;
 use App\http\Controllers\ChirpController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LikeController;
+
 
 Route::get('/', function () {
     return view('welcome');
@@ -11,6 +13,10 @@ Route::get('/', function () {
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::post('/chirps/{chirp}/like', [LikeController::class, 'store'])->name('chirps.like');
+Route::delete('/chirps/{chirp}/like', [LikeController::class, 'destroy'])->name('chirps.unlike');
+
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
