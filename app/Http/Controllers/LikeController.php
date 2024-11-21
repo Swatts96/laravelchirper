c9e3f8a6f14244cf6ee83f1e9970bcb8d4a4b7d6
Sam <w0292233@nscc.ca>
2024-11-21 18:54:05 -0400

    added functionality to controller for adding likes and removing

--- app/Http/Controllers/LikeController.php
+++ app/Http/Controllers/LikeController.php
@@ -8,12 +8,20 @@ use Illuminate\Http\Request;
 class LikeController extends Controller
 {
     // Storing a like
-    public function liking(Request $request, Chirp $chirp){
-
+    public function store(Request $request, Chirp $chirp): \Illuminate\Http\RedirectResponse
+    {
+        // adding the like
+        $chirp->likes()->create([
+            'user_id' => auth()->id(),
+        ]);
+        return redirect()->back()->with('success', 'Chirp liked!');
     }
 
     // Removing a like
-    public function destroy(Request $request, Chirp $chirp){
-
+    public function destroy(Request $request, Chirp $chirp): \Illuminate\Http\RedirectResponse
+    {
+        // Find the like and delete it
+        $chirp->likes()->where('user_id', auth()->id())->delete();
+        return redirect()->back()->with('success', 'Chirp unliked!');
     }
 }
